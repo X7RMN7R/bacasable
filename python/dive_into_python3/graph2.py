54e9531af21ba91e1aa7c698afdf62569d4ab85a
X7RMN7R <uv0.xtr@gmail.com>
2020-04-09 17:55:35 +0200

    Mandelbrot tries with Python

---
+++ python/dive_into_python3/graph2.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import mandelbrot as mb
+
+size = 300
+rangeX = np.linspace(-2,0.5,size)
+rangeY = np.linspace(-1.5,1.5,size)
+
+resX = []
+resY = []
+
+def showPoint(x, y):
+	z = (x,y)
+	nthElement = mb.mandelbrot(z, 10)
+	return mb.zdist(nthElement) < 10
+	
+for x in rangeX:
+	for y in rangeY:
+		if showPoint(x,y):
+			resX.append(x)
+			resY.append(y)
+
+plt.scatter(resX,resY,1)
+
+plt.xlabel('x - axis')
+plt.ylabel('y - axis')
+
+plt.title('Ensemble de Mandelbrot')
+
+plt.show()
---
+++ python/dive_into_python3/mandelbrot.py
@@ -0,0 +1,20 @@
+import math
+
+def zadd(z1, z2):
+	return (z1[0] + z2[0], z1[1] + z2[1])
+	
+def zmult(z1, z2):
+	return (z1[0]*z2[0] - z1[1]*z2[1], z1[0]*z2[1] + z1[1]*z2[0])
+	
+def zsquare(z):
+	return zmult(z, z)
+
+def zdist(z):
+	return math.sqrt(z[0]*z[0] + z[1]*z[1])
+	
+def mandelbrot(c,n):
+	if n == 0:
+		return (0, 0)
+	else:
+		return zadd(zsquare(mandelbrot(c,n-1)), c)
+
